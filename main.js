9164632b47a7afcfae89bbb6f340abf29bae3bfc
IntScientist <153201105+raphraphra@users.noreply.github.com>
2024-03-05 09:45:50 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,15 @@
+
+// discord.js classes
+
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+
+client.once(Events.ClientReady, readyClient => {
+	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+client.login(token);
+
